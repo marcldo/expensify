1b292843d6502d44c8fc563514565dc5947b76da
gitMarcldo <marcldo@users.noreply.github.com>
2021-03-08 14:30:29 -0500

    selectors expenses test sort by date

--- src/tests/selectors/expenses.test.js
+++ src/tests/selectors/expenses.test.js
@@ -54,3 +54,14 @@ test('should filter by end date', () => {
   const result = selectExpenses(expenses, filters);
   expect(result).toEqual([expenses[0], expenses[1]]);
 });
+
+test('should sort by date', () => {
+  const filters = {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+  }
+  const result = selectExpenses(expenses, filters);
+  expect(result).toEqual([expenses[2], expenses[0], expenses[1]]);
+});
