ee7f39654f7f8e7074b96a33563530a57c2ac9d8
gitMarcldo <marcldo@users.noreply.github.com>
2021-03-09 12:33:29 -0500

    filters reducer test, start date

--- src/tests/reducers/filters.test.js
+++ src/tests/reducers/filters.test.js
@@ -32,11 +32,21 @@ test('should set sort by to date', () => {
 });
 
 test('should set text filter', () => {
-  const text = 'This is test value'
+  const text = 'This is test value';
   const action = {
     type: 'SET_TEXT_FILTER',
     text
-  }
+  };
   const state = filtersReducer(undefined, action);
   expect(state.text).toBe(text);
+});
+
+test('should set start date filter', () => {
+  const startDate = moment();
+  const action = {
+    type: 'SET_START_DATE',
+    startDate
+  };
+  const state = filtersReducer(undefined, action);
+  expect(state.startDate).toEqual(startDate);
 });
