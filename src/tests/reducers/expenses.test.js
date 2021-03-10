2e37f05476e67cbff5410179b1cd325aee77270b
gitMarcldo <marcldo@users.noreply.github.com>
2021-03-10 13:31:12 -0500

    expenses reducer test, default and remove expense

---
+++ src/tests/reducers/expenses.test.js
@@ -0,0 +1,17 @@
+import expensesReducer from '../../reducers/expenses';
+import expenses from '../fixtures/expenses';
+
+test('should set default state', () => {
+  const state = expensesReducer(undefined, { tyoe: '@@INIT' });
+  expect(state).toEqual([]);
+});
+
+test('should remove expense by id', () => {
+  const action = {
+    type: 'REMOVE_EXPENSE',
+    id: expenses[1].id
+  }
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual([expenses[0], expenses[2]]);
+});
+
