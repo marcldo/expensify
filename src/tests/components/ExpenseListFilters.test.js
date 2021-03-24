ea674ac64cf3d9b9c312707d7b78a10649e5e362
gitMarcldo <marcldo@users.noreply.github.com>
2021-03-24 12:07:32 -0400

    component test: expenseListFilters calendarFocused

--- src/tests/components/ExpenseListFilters.test.js
+++ src/tests/components/ExpenseListFilters.test.js
@@ -73,5 +73,7 @@ test('should handle date change', () => {
 });
 
 test('should handle date focus change', () => {
-
+  const calendarFocused = 'endDate';
+  wrapper.find('DateRangePicker').prop('onFocusChange')(calendarFocused);
+  expect(wrapper.state('calendarFocused')).toBe(calendarFocused);
 });
