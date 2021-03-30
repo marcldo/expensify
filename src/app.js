4e142846c86c468e522bb0217dbb4ffd038bb18e
gitMarcldo <marcldo@users.noreply.github.com>
2021-03-30 14:27:25 -0400

    Add expense summary to dashboard

--- src/app.js
+++ src/app.js
@@ -11,15 +11,7 @@ import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
-store.dispatch(addExpense({ description: 'Water Bill', amount: 4500 }));
-store.dispatch(addExpense({ description: 'Gas Bill', createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
 
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-
-console.log(store.getState());
-console.log(visibleExpenses);
 
 const jsx = (
   <Provider store={store}>
