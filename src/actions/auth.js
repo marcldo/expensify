13026edb8a97a9ec81c2480f7562c33c39da048c
gitMarcldo <marcldo@users.noreply.github.com>
2021-04-19 12:04:17 -0400

    login and logout auth reducers and actions

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,4 +1,13 @@
-import { firebase, googleAuthProvider } from '../firebase/firebase'
+import { firebase, googleAuthProvider } from '../firebase/firebase';
+
+export const login = (uid) => ({
+  type: 'LOGIN',
+  uid
+});
+
+export const logout = () => ({
+  type: 'LOGOUT'
+});
 
 export const startLogin = () => {
   return () => {
